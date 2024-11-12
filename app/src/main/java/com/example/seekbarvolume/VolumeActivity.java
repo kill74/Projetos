6a1a4aae0909de9da52c64bbd927fd71b16dd567
Guiii <gnailpo895@gmail.com>
2024-11-12 19:38:16 +0000

    First Commit

---
+++ app/src/main/java/com/example/seekbarvolume/VolumeActivity.java
@@ -0,0 +1,52 @@
+package com.example.seekbarvolume;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class VolumeActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_volume);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        }); // Fechei aqui pois estava a dar conflito com o codigo em baixo
+            // mais especificamente com o "seekBar" e o "textView"
+
+            seekBar = findViewById(R.id.seekBar);
+            textView = findViewById(R.id.textView);
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    textView.setText(String.valueOf(progress));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+
+        });
+    }
+}
